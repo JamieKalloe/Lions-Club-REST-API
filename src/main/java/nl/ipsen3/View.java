47c452bc1deffea2f3596ba44c7fd958947afc2f
Phillywonka <philip_wong@live.nl>
2016-01-14 18:59:13 +0100

    merge master

---
+++ src/main/java/nl/ipsen3/View.java
@@ -0,0 +1,15 @@
+package nl.ipsen3;
+
+/**
+ * Meer informatie over views:
+ *  http://www.baeldung.com/jackson-json-view-annotation
+ * 
+ * @author Groep1>
+ * @since 1.0
+ */
+public class View
+{
+    public static class Private extends Public {}
+    
+    public static class Public {}
+}
