a0271c13227fcfd2e94c6469292fe92ca6e2a840
JamieKalloe <Jamie@Kalloe.net>
2016-01-18 12:37:28 +0100

    added WineService and WineResource

---
+++ src/main/java/nl/ipsen3/resource/WineResource.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.ipsen3.resource;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import java.util.Collection;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import nl.IPSEN3.model.Wine;
+import nl.ipsen3.View;
+import nl.ipsen3.model.User;
+import nl.ipsen3.service.UserService;
+import nl.ipsen3.service.WineService;
+
+/**
+ *
+ * @author Jamie
+ */
+
+@Path("/wines")
+@Produces(MediaType.APPLICATION_JSON)
+public class WineResource {
+    
+    private final WineService service;
+    
+    public WineResource(WineService service)
+    {
+        this.service = service;
+    }
+    
+    @GET
+    @JsonView(View.Public.class)
+    public Collection<Wine> retrieveAll()
+    {
+        return service.getAll();
+    }
+    
+    @GET
+    @Path("/{id}")
+    @JsonView(View.Public.class)
+    public Wine retrieve(@PathParam("id") int id)
+    {
+        return service.get(id);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JsonView(View.Private.class)
+    public void create(Wine wine)
+    {
+        service.add(wine);
+    }
+    
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JsonView(View.Private.class)
+    public void update(@PathParam("id") int id, Wine wine)
+    {
+        service.update(id, wine);
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public void delete(@PathParam("id") int id)
+    {
+        service.delete(id);
+    }
+}
