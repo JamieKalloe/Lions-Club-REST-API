8431469c62f22cafcbd03b84a9df8a234736bb73
Koen Riksen <koen.zyx@gmail.com>
2016-01-20 14:24:54 +0100

    simplified a few lines of code in the orderdao and order model

--- src/main/java/nl/ipsen3/model/Order.java
+++ src/main/java/nl/ipsen3/model/Order.java
@@ -20,7 +20,7 @@ public class Order {
     private int id;
     
     @JsonView(View.Public.class)
-    private User user;
+    private int userId;
     
     @JsonView(View.Public.class)
     private OrderStatus orderStatus;
@@ -36,7 +36,7 @@ public class Order {
     
     public Order(int id, int userId, int orderStatusId) {
         this.id = id;
-        this.user = new User(userId);
+        this.userId = userId;
         this.orderStatus = new OrderStatus(orderStatusId);
         this.totalAmount = 0;
         this.wines = null;
@@ -56,8 +56,8 @@ public class Order {
         return id;
     }
 
-    public User getUser() {
-        return user;
+    public int getUserId() {
+        return userId;
     }
 
     public OrderStatus getOrderStatus() {
@@ -72,8 +72,8 @@ public class Order {
         this.id = id;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
--- src/main/java/nl/ipsen3/persistence/OrderDAO.java
+++ src/main/java/nl/ipsen3/persistence/OrderDAO.java
@@ -92,7 +92,7 @@ public class OrderDAO {
         HashMap databaseData = new HashMap();
         
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        databaseData.put("guest_id", order.getUser().getId());
+        databaseData.put("guest_id", order.getUserId());
         databaseData.put("status_id", order.getOrderStatus().getId());
         databaseData.put("order_date", sdf.format(new Date()));
         
@@ -102,7 +102,7 @@ public class OrderDAO {
     private void updateOrderFromDatabase(Order order) {
         HashMap databaseData = new HashMap();
         
-        databaseData.put("guest_id", order.getUser().getId());
+        databaseData.put("guest_id", order.getUserId());
         databaseData.put("status_id", order.getOrderStatus().getId());
         
         databaseInstance.update("order", order.getId(), databaseData);
