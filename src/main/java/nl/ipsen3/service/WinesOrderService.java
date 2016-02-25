08a8b52d862f1769de31ad21ae7ecc1907d1290b
JamieKalloe <Jamie@Kalloe.net>
2016-02-25 14:09:40 +0100

    added new WinesOrder dao and service

---
+++ src/main/java/nl/ipsen3/service/WinesOrderService.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.ipsen3.service;
+
+import java.util.Collection;
+import nl.ipsen3.model.WineOrder;
+import nl.ipsen3.persistence.WineOrderDAO;
+
+/**
+ *
+ * @author Jamie
+ */
+public class WinesOrderService extends BaseService<WineOrder> {
+    
+    private final WineOrderDAO dao;
+    
+    public WinesOrderService(WineOrderDAO dao) {
+        this.dao = dao;
+    }
+    
+    public Collection<WineOrder> getAll() {
+        return dao.getAll();
+    }
+    
+    public WineOrder get(int id) {
+        return requireResult(dao.get(id));
+    }
+    
+    public void add(WineOrder wineOrder) {
+        dao.add(wineOrder);
+    }
+    
+    public void update(int id, WineOrder wineOrder) {
+        WineOrder oldWineOrder = get(id);
+        
+        dao.update(id, wineOrder);
+    }
+    
+    public void delete(int id) {
+        WineOrder wineOrder = get(id);
+        
+        dao.delete(id);
+    }
+}
